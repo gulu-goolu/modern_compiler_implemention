a3647ac4581e3292a254091e658265486ffe5db9
caohongjian <murmur.wheel@gmail.com>
2021-05-08 20:56:20 +0800

    refactor: 用 cmake 重构构建系统

---
+++ tiger/chap4/parse.cpp
@@ -0,0 +1,23 @@
+/*
+ * parse.c - Parse source file.
+ */
+
+#include "parse.h"
+#include <stdio.h>
+#include "absyn.h"
+#include "errormsg.h"
+#include "symbol.h"
+#include "util.h"
+
+extern int yyparse(void);
+extern A_exp absyn_root;
+
+/* parse source file fname;
+   return abstract syntax data structure */
+A_exp parse(string fname) {
+  EM_reset(fname);
+  if (yyparse() == 0) /* parsing worked */
+    return absyn_root;
+  else
+    return NULL;
+}
